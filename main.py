3443dfffe0c96417d4600fb7c3ed224f5ef2e179
zenfry <jwilkes@thegraysonschool.org>
2025-03-18 08:46:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ from newton import newton
 import numpy as np
 import matplotlib.pyplot as plt
 
-def test_quad(x):
-    return x**2 - 1
+def test_cubic(z):
+    return z**3 - 0.00004
 
-def test_quad_prime(x):
-    return 2*x
+def test_cubic_prime(z):
+    return 3*z**2
 
 def main():
-    xs = np.linspace(-2, 2, 101)
-    ys = test_quad(xs)
-    roots = newton(test_quad, xs, fprime=test_quad_prime)
 
-    plt.plot(xs, ys, label="Test Quadratic")
-    plt.plot(xs, roots, label="Root found with Newton's method")
-    plt.legend()
+    xs = np.linspace(-2, 2, 700)
+    ys = np.linspace(-2, 2, 700)
+    zs = xs[:, None] + 1j * ys  
+
+    
+    roots = newton(test_cubic, zs, fprime=test_cubic_prime)
+
+    
+    unique_roots = np.unique(np.round(roots, decimals=6))
+    root_map = {root: i for i, root in enumerate(unique_roots)}
+    colors = np.vectorize(lambda r: root_map.get(np.round(r, decimals=6), -1))(roots)
+
+    plt.figure(figsize=(6, 6))
+    plt.imshow(colors, extent=(-2.5, 2.5, -2.5, 2.5), origin='lower', cmap='hsv')
+    plt.title("Newton Fractal 1")
+    plt.grid(True, linestyle='--', linewidth=0.5)
     plt.show()
 
 if __name__ == '__main__':
